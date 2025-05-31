a6e76c9977c0e6f16f084bddab687e37fd6170ce
jeswin <jeswinpeter92@gmail.com>
2025-05-31 12:55:56 +0530

    In progress

---
+++ Arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int sum_of_elements(int aray[],int size) {
+    for int i ;
+}
+
+int main() {
+
+    return 0;
+}
---
+++ Calcu.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num1, num2;
+    cout << "Enter First number: ";
+    cin >> num1;
+
+    cout << "Enter Second number: ";
+    cin >> num2;
+
+    cout << "The sum of the numbers you entered is => " << num1 + num2;
+
+    return 0;
+}
