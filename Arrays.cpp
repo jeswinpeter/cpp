af268416d1ce9c498a6e9fddbad2528fd5ae26c2
jeswin <jeswinpeter92@gmail.com>
2025-05-31 18:48:57 +0530

    Completed

--- Arrays.cpp
+++ Arrays.cpp
@@ -3,10 +3,28 @@
 using namespace std;
 
 int sum_of_elements(int aray[],int size) {
-    for int i ;
+    int sum = 0;
+    for(int i = 0; i < size; i++) {
+        sum += aray[i];
+    }
+
+    return sum;
 }
 
 int main() {
+    int size; 
+    int array[10];
 
+    // Getting user input 
+    cout << "Enter the size of array: ";
+    cin >> size;
+    cout << "Enter the numbers: ";
+    for(int i = 0; i < size; i++){
+        cin >> array[i];
+    }
+
+    // Calling function and printing output
+    cout << "The sum of the numbers you entered is >> " << sum_of_elements(array, size);
+    
     return 0;
 }
---
+++ if_else.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    char response;
+
+    cout << "Are you a Human, enter y for Yes and n for No: ";
+    cin >> response;
+
+    if(response == 'y') {
+        cout << "User might be Human";
+    }
+
+    else if(response == 'n') {
+        cout << "User is Human for sure :)";
+    }
+
+    else {
+        cout << "\n\t!!!! ALIEN FOUND !!!!\n";
+    }
+
+
+    return 0;
+}
